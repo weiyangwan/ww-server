9775396c3459f54dd273e81bac3d9f9b3fd5d0da
weiyang wan <weiyangwan@gmail.com>
2017-01-09 16:12:22 +0800

    amend mongodb for heroku

--- config/express.js
+++ config/express.js
@@ -23,7 +23,7 @@ module.exports = function() {
 
   app.use(function(req,res,next){
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "DELETE,PULL,PATCH");
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,PULL,PATCH");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
   });
